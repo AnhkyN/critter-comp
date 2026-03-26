89ec894f4e2f45e3a8416029ff1e4b7ac918137c
AnhkyN <anhkynguyen56@gmail.com>
2026-03-25 20:52:31 -0700

    colors

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 export default function Home() {
   return (
-    <main className="min-h-screen bg-[#f4f6f3] flex items-center justify-center">
+    <main className="min-h-screen bg-[#f3ffc7] flex items-center justify-center">
       <h1 className="text-6xl font-oi text-[#24ed4b] drop-shadow-[2px_2px_0px_rgb(0,0,0)]">
         Critter Comp
       </h1>
